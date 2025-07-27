f205fb66075921abe936e439cc44067618cba098
hamzathul dilshad <diluhd99@gmail.com>
2025-07-28 02:25:40 +0530

    Env Fix forntend

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const env={
-    apiUrl:`http://localhost:5000/api`,
-    baseUrl:'http://localhost:5000',
+    apiUrl:process.env['NG_APP_API_URL'] || `http://localhost:5000/api`,
+    baseUrl:process.env['NG_APP_BASE_URL'] || `http://localhost:5000`,
 }
